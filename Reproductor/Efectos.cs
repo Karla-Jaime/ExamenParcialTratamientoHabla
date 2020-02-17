03d08bfa8381a90e48d7f0cfbb57c30db9027978
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2020-02-17 12:14:35 -0700

    Agregar archivos de proyecto.

---
+++ Reproductor/Efectos.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NAudio.Wave;
+
+namespace Reproductor
+{
+    class Efectos : ISampleProvider
+    {
+        private ISampleProvider fuente;
+        private int muestrasLeidas = 0;
+        private float segundosTranscurridos = 0;
+        private float inicio;
+        private float duracion;
+        private float duracion2;
+
+        public Efectos(ISampleProvider fuente,
+            float inicio,
+            float duracion, float duracion2)
+        {
+            this.inicio = inicio;
+            this.fuente = fuente;
+            this.duracion = duracion;
+            this.duracion2 = duracion2;
+
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get
+            {
+                return fuente.WaveFormat;
+            }
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            int read = fuente.Read(buffer, offset, count);
+
+
+            muestrasLeidas += read;
+            segundosTranscurridos =
+                (float)muestrasLeidas /
+                (float)fuente.WaveFormat.SampleRate /
+                (float)fuente.WaveFormat.Channels;
+
+            if (segundosTranscurridos <= duracion)
+            {
+                //Aplicar el efecto
+                float factorEscala =
+                    segundosTranscurridos /
+                        duracion;
+                for (int i = 0; i < read; i++)
+                {
+                    buffer[i + offset] *=
+                        factorEscala;
+                }
+            }
+
+            if (segundosTranscurridos >= inicio &&
+                segundosTranscurridos <= inicio + duracion2)
+            {
+                //Aplicar el efecto
+                float factorEscala =
+                    1 - ((segundosTranscurridos - inicio) /
+                        duracion);
+                for (int i = 0; i < read; i++)
+                {
+                    buffer[i + offset] *=
+                        factorEscala;
+                }
+            }
+            else if (segundosTranscurridos >= inicio + duracion2)
+            {
+                for (int i = 0; i < read; i++)
+                {
+                    buffer[i + offset] = 0.0f;
+                }
+            }
+
+            return read;
+        }
+    }
+}
